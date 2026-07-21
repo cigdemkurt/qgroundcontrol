b919058a57a8df4e568d5d8f35dbfd0baad65c58
Çiğdem <121744920+cigdemkurt@users.noreply.github.com>
2026-07-21 14:40:13 +0000

    custom: uygulama adindan bosluk kaldirildi

--- custom/cmake/CustomOverrides.cmake
+++ custom/cmake/CustomOverrides.cmake
@@ -1,2 +1,2 @@
-# Uygulama adi - istediginiz gibi degistirin
-set(QGC_APP_NAME "H12 QGC" CACHE STRING "App Name" FORCE)
+# Uygulama adi - CMake hedef adi olarak da kullanildigi icin bosluk icermemeli
+set(QGC_APP_NAME "H12QGC" CACHE STRING "App Name" FORCE)
